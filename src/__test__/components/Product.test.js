6f1a60dbf0b0316f83c8d419a2c493424f9e4e5e
David Yzaguirre <dgonzalez@gisystemsint.com>
2020-06-26 16:09:44 -0600

    Fix

--- src/__test__/components/Product.test.js
+++ src/__test__/components/Product.test.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { mount, shallow } from "enzyme";
 import ProviderMock from "../../__mocks__/ProviderMock";
 import ProductMock from '../../__mocks__/components/ProductMock';
